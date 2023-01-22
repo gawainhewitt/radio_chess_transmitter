f445eaa7afa9c432f10bc9aa0e1ccd2364e076a7
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2023-01-22 09:08:01 +0000

    reads RFID most times and serial prints boolean for state

--- radio_chess_transmitter.ino
+++ radio_chess_transmitter.ino
@@ -1,29 +1,64 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
+#include "rfid.h"
 
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+bool pieceState = false;
 
-RF24 radio(7, 8); // CE, CSN
+struct rfidStatus  // to keep the details of each card in
+{
+  unsigned long ID = 0;
+  int state = 0;
+};
 
-const byte address[6] = "00001";
+rfidStatus readers[NR_OF_READERS]; //create an array of currentRFID objects
 
 void setup() {
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+  init_rfid();
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+  bool checkState = false;
+
+  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++)
+  {
+    int rfid_removed[NR_OF_READERS];    //create an array to store values in
+
+    rfid_removed[reader] = checkRFIDremoved(reader); // call the check RFID function and store returned value in the array
+
+    if (rfid_removed[reader] != -1)   // if the above function doesn't have a card removed then it will return -1 and we won't do the rest
+      {
+        readerState[reader]= false;
+        readers[reader].state = 0;
+      }
+
+    int rfid_added[NR_OF_READERS];  //create an array to store values in
+
+    rfid_added[reader] = checkRFIDadded(reader); // call the check RFID function and store returned value in the array
+
+    if (rfid_added[reader] != -1) // if the above function doesn't have a card added then it will return -1 and we won't do the rest
+      {
+        readerState[reader] = true;
+        readers[reader].state = 1;
+        readers[reader].ID = rfid_added[reader];
+      }
+
+    if (readerState[reader] != storedState[reader]){
+      if (readerState[reader] == 1) {
+        storedState[reader] = readerState[reader];
+      } else {
+        storedState[reader] = readerState[reader];
+      }
+    }
+
+    if (storedState[reader] == true) {
+      checkState = storedState[reader];
+    }
+  }
+
+  if (checkState != pieceState){
+    pieceState = checkState;
+    Serial.println(pieceState);
+  }
+
 }
